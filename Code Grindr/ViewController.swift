b73166b4a71c9dcfd9ea7338d513b1d2e298e201
nhatduy227 <53373898+nhatduy227@users.noreply.github.com>
2020-03-19 14:39:27 -0400

    Initial Commit

---
+++ Code Grindr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Code Grindr
+//
+//  Created by Nom Phan on 3/19/20.
+//  Copyright © 2020 Nom Phan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
